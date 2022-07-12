525b6cc82c3047371ccf87fd5a044b73d0cd737b
vladimirsibichenkov <97192143+VladiSibichen@users.noreply.github.com>
2022-07-12 19:29:55 +0300

    Starting
    Just start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src={'https://www.edigitalagency.com.au/wp-content/uploads/Facebook-logo-blue-circle-large-transparent-png.png'}></img>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src={'https://lucknowrecreation.com/wp-content/uploads/2018/04/Monthly-Website-Header-background-e1522931579825.jpg'}/>
+            </div>
+            <div>
+                ava + discription
+                <img></img>
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Profile;
